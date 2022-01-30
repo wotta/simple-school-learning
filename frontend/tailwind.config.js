cb2f7021eeb7d457204eda1d9244c4644eaf0817
Wouter <wouter.van.marrum@protonmail.com>
2022-01-30 15:51:17 +0100

    Added tailwind/forms to the frontend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -27,5 +27,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 }
