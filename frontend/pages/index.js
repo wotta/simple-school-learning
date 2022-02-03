c1f9f76ff119022d1aa239ec2372c1c54b07a6ea
Wouter <wouter.van.marrum@protonmail.com>
2022-02-03 22:08:00 +0100

    Removed code that we don't use.

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,9 @@
-import Image from "next/image"
 import { format } from "date-fns"
 import Seo from "../components/seo"
 import { fetchAPI } from "../lib/api"
 import { GlobalContext } from "./_app"
 import Layout from "../components/layout"
 import React, { useContext } from "react"
-import ReactMarkdown from "react-markdown"
 import Courses from "../components/courses"
 
 const Home = ({ homepage, courses }) => {
@@ -20,37 +18,6 @@ const Home = ({ homepage, courses }) => {
       <Seo seo={homepage.attributes.seo} />
       <div className="container mt-24 md:mt-18 p-8 rounded prose prose-pink prose-sm sm:prose-lg lg:prose-lg xl:prose-2xl mx-auto">
         <main>
-          <article>
-            <ReactMarkdown
-              components={{
-                p: function p({ node, children }) {
-                  if (node.children[0].tagName === "img") {
-                    let image = node.children[0]
-
-                    let imageProps = {
-                      ...image.properties,
-                    }
-
-                    return (
-                      <div
-                        className="
-          flex w-32 h-32 rounded-full mr-5 ml-3 float-right place-content-start mt-0 overflow-hidden
-          [shape-outside: circle(50% at 50% 50%)]
-        "
-                      >
-                        <Image {...imageProps} width={200} height={200} />
-                      </div>
-                    )
-                  }
-
-                  return <p>{children}</p>
-                },
-              }}
-            >
-              {content.content}
-            </ReactMarkdown>
-          </article>
-
           <Courses courses={courses} />
         </main>
       </div>
@@ -78,14 +45,14 @@ export async function getStaticProps() {
         $or: [
           {
             end_date: {
-              "$gte": date,
-            }
+              $gte: date,
+            },
           },
           {
-            end_date: { $null: true }
+            end_date: { $null: true },
           },
-        ]
-      }
+        ],
+      },
     }),
   ])
 
